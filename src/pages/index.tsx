f888fcca786f4947e19f0044e3ad07a0e505851e
Guilherme Costa <guilherme.rodrigues.costa@outlook.com>
2021-12-30 19:32:45 -0300

    chore: add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "@/pages/index.module.css";
+
+export default function Home() {
+  return <div>Hello world</div>;
+}
