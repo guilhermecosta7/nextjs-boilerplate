724886f2387298c01627df31c80bd930c3df3283
Guilherme Costa <guilherme.rodrigues.costa@outlook.com>
2021-12-30 19:42:43 -0300

    chore: add prettier and rules/eslint-config-prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from 'next/head'
+import Image from 'next/image'
 
-import styles from "@/pages/index.module.css";
+import styles from '@/pages/index.module.css'
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return <div>Hello world</div>
 }
