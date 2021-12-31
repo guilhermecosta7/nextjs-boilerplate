7330902f2e06dffc0c8c043ef3001ac0c34bcefa
Guilherme Costa <guilherme.rodrigues.costa@outlook.com>
2021-12-31 11:53:21 -0300

    chore: change absolute import path in tsconfig.json from . to src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import GlobalStyles from 'src/styles/global'
+import GlobalStyles from 'styles/global'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
